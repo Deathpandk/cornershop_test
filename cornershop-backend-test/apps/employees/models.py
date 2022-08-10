a9633ecd62e5b89f4e0bb3e7ddcc891b41c80347
Joseph Sierra <joseph.sgtz@gmail.com>
2022-08-09 20:18:58 -0500

    Orders model

--- cornershop-backend-test/apps/employees/models.py
+++ cornershop-backend-test/apps/employees/models.py
@@ -6,3 +6,16 @@ class Employee(models.Model):
 
     name = models.CharField(max_length=32)
     slack_id = models.CharField(max_length=16, unique=True)
+
+
+class Order(models.Model):
+    """Model to register employee menu election"""
+
+    employee = models.ForeignKey("employees.Employee", on_delete=models.CASCADE)
+    menu = models.ForeignKey("menu.Menu", on_delete=models.PROTECT)
+    option = models.ForeignKey("menu.MenuOption", on_delete=models.PROTECT)
+
+    comments = models.CharField(max_length=512, null=True, blank=True)
+
+    class Meta:
+        unique_together = ["menu", "employee"]
--- cornershop-backend-test/apps/employees/tests/test_models.py
+++ cornershop-backend-test/apps/employees/tests/test_models.py
@@ -2,7 +2,8 @@ from django.db import DataError, IntegrityError
 from django.test import TestCase
 
 from apps.employees.factories import EmployeeFactory
-from apps.employees.models import Employee
+from apps.employees.models import Employee, Order
+from apps.menu.factories import MenuOptionFactory
 
 
 class EmployeeModelTest(TestCase):
@@ -36,3 +37,50 @@ class EmployeeModelTest(TestCase):
         with self.assertRaises(DataError) as error:
             Employee.objects.create(name="N" * 33, slack_id="ID")
         self.assertIn("value too long", str(error.exception))
+
+
+class OrderModelTest(TestCase):
+    """Tests for Order Model"""
+
+    def setUp(self):
+        self.option = MenuOptionFactory()
+        self.employee = EmployeeFactory()
+
+    def test_create_order(self):
+        """Test create order"""
+
+        Order.objects.create(
+            menu=self.option.menu,
+            option=self.option,
+            employee=self.employee,
+            comments="With salad",
+        )
+        self.assertTrue(Order.objects.exists())
+
+    def test_create_order_without_comments(self):
+        """Test create order with no comments"""
+
+        Order.objects.create(
+            menu=self.option.menu,
+            option=self.option,
+            employee=self.employee,
+        )
+        self.assertTrue(Order.objects.exists())
+
+    def test_create_order_with_existing_employee_menu(self):
+        """Test raises error when creating order with existing employee-menu"""
+
+        Order.objects.create(
+            menu=self.option.menu,
+            option=self.option,
+            employee=self.employee,
+            comments="With salad",
+        )
+        with self.assertRaises(IntegrityError) as error:
+            Order.objects.create(
+                menu=self.option.menu,
+                option=self.option,
+                employee=self.employee,
+                comments="With salad",
+            )
+        self.assertIn("duplicate key value", str(error.exception))
